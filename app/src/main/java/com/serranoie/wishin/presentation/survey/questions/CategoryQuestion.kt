7095c9faa58e9a8d5d3461cf054a74a6ecf4931c
Isaac Serrano <serranoie99@gmail.com>
2024-02-03 22:06:25 -0700

    Implemented CategoryQuestion screen

--- app/src/main/java/com/serranoie/wishin/presentation/survey/questions/CategoryQuestion.kt
+++ app/src/main/java/com/serranoie/wishin/presentation/survey/questions/CategoryQuestion.kt
@@ -1,8 +1,36 @@
 package com.serranoie.wishin.presentation.survey.questions
 
+import android.content.res.Configuration
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.serranoie.wishin.R
+import com.serranoie.wishin.presentation.survey.QuestionWrapper
+import com.serranoie.wishin.presentation.utils.Dimens.smallPadding
+import com.serranoie.wishin.ui.theme.WishinTheme
 
 @Composable
 fun CategoryQuestion(
@@ -13,6 +41,97 @@ fun CategoryQuestion(
     onOptionSelected: (Category) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    QuestionWrapper(
+        titleResourceId = titleResourceId,
+        directionsResourceId = directionsResourceId,
+        modifier = modifier.selectableGroup(),
+    ) {
+        possibleAnswers.forEach {
+            val selected = it == selectedAnswer
+
+            RadioButtonOption(
+                modifier = modifier.padding(vertical = smallPadding),
+                text = stringResource(id = it.name),
+                selected = selected,
+                onOptionSelected = { onOptionSelected(it) },
+            )
+        }
+    }
 }
 
-data class Category(val name: String)
+@Composable
+fun RadioButtonOption(
+    text: String,
+    selected: Boolean,
+    onOptionSelected: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        shape = MaterialTheme.shapes.small,
+        color = if (selected) {
+            MaterialTheme.colorScheme.primaryContainer
+        } else {
+            MaterialTheme.colorScheme.surface
+        },
+        border = BorderStroke(
+            width = 1.dp,
+            color = if (selected) {
+                MaterialTheme.colorScheme.primary
+            } else {
+                MaterialTheme.colorScheme.outline
+            },
+        ),
+        modifier = modifier
+            .clip(MaterialTheme.shapes.small)
+            .selectable(
+                selected,
+                onClick = onOptionSelected,
+                role = Role.RadioButton,
+            ),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Spacer(Modifier.width(8.dp))
+
+            Text(text, Modifier.weight(1f), style = MaterialTheme.typography.titleLarge)
+            Box(Modifier.padding(8.dp)) {
+                RadioButton(selected, onClick = null)
+            }
+        }
+    }
+}
+
+@Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun CategoryQuestionPreview() {
+    val possibleAnswers = listOf(
+        Category(R.string.category_art),
+        Category(R.string.category_automotive),
+        Category(R.string.category_beauty),
+        Category(R.string.category_books),
+        Category(R.string.category_clothing),
+        Category(R.string.category_electronics),
+        Category(R.string.category_gaming),
+        Category(R.string.category_hobbies),
+        Category(R.string.category_tools),
+    )
+    var selectedAnswer by remember { mutableStateOf<Category?>(null) }
+
+    WishinTheme {
+        Surface {
+            CategoryQuestion(
+                titleResourceId = R.string.question,
+                directionsResourceId = R.string.instructions,
+                possibleAnswers = possibleAnswers,
+                selectedAnswer = selectedAnswer,
+                onOptionSelected = { selectedAnswer = it },
+            )
+        }
+    }
+}
+data class Category(val name: Int)
