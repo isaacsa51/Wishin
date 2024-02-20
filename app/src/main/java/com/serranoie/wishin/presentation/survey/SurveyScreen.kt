71ac4db0e3d11f79651e3afc62deef91cf82109d
Isaac Serrano <serranoie99@gmail.com>
2024-02-19 22:49:39 -0700

    Minor change at SurveyScreen

--- app/src/main/java/com/serranoie/wishin/presentation/survey/SurveyScreen.kt
+++ app/src/main/java/com/serranoie/wishin/presentation/survey/SurveyScreen.kt
@@ -7,11 +7,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -82,7 +81,7 @@ fun SurveyResultScreen(
                 )
             },
             bottomBar = {
-                OutlinedButton(
+                Button(
                     onClick = onDonePressed,
                     modifier = Modifier
                         .fillMaxWidth()
