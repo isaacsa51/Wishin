df6dcb743185ab7f25c59e76324ab94147dbc288
Isaac Serrano <serranoie99@gmail.com>
2024-02-21 17:23:53 -0700

    Hotfix, added missing annotations to RepositoryModule

--- app/src/main/java/com/serranoie/wishin/di/RepositoryModule.kt
+++ app/src/main/java/com/serranoie/wishin/di/RepositoryModule.kt
@@ -1,12 +1,17 @@
 package com.serranoie.wishin.di
 
 import com.serranoie.wishin.data.repositories.ItemRepositoryImpl
+import com.serranoie.wishin.domain.repositories.ItemRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-    abstract fun bindRepository(repositoryImpl: ItemRepositoryImpl)
+    @Binds
+    @Singleton
+    abstract fun bindRepository(repositoryImpl: ItemRepositoryImpl): ItemRepository
 }
